d4e7b8495df9fb7a32ddd27ddfab1649e173aeb8
Anil Poudyal <anilrunjakote@gmail.com>
2019-12-21 01:16:14 +0545

    Some tweaks in models and views

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogsPost(models.Model):
+    blog_title = models.CharField(max_length=200)
+    blog_content = models.TextField()
+    blog_img = models.CharField(max_length=2083)
+    blog_author = models.CharField(max_length=200)
